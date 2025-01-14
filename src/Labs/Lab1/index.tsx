25822754f63e7bf2204bafb9a190bcf3f91aad19
sychan91 <c.saiyin@gmail.com>
2025-01-14 15:26:05 -0500

    finished subsection 1.1.3

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,6 +1,6 @@
 export default function Lab1() {
     return (
-        <div>
+        <div id="wd-lab1">
             <h2>Lab 1</h2>
         </div>
     );
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,7 +1,7 @@
 import Lab1 from "./Lab1";
 export default function Labs() {
     return (
-        <div>
+        <div id="wd-labs">
             <h1>Labs</h1>
             <Lab1 />
         </div>
