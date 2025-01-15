5d2cad1866c41d9c9b4b9219a40a50c4f94ccb65
sychan91 <c.saiyin@gmail.com>
2025-01-15 15:33:07 -0500

    Kambaz landing page created, made default and added to TOC

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Labs from "./Labs";
+import Kambaz from "./Kambaz";
 import { HashRouter, Route, Routes, Navigate } from "react-router";
 
 function App() {
@@ -6,8 +7,9 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/" element={<Navigate to="Kambaz" />} />
           <Route path="/Labs/*" element={<Labs />}/>
+          <Route path="/Kambaz/*" element={<Kambaz />}/>
         </Routes>
       </div>
     </HashRouter>
