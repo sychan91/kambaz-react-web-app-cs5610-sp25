21714d44b32bfd5df4598c6eff26328810a3b322
sychan91 <c.saiyin@gmail.com>
2025-01-14 21:23:13 -0500

    1.2.5 Table Tags completed

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -82,6 +82,87 @@ export default function Lab1() {
                     <li>Animal Farm</li>
                 </ul>
             </div>
+            <div id="wd-tables">
+                <h4>Table Tag</h4>
+                <table border={1} width="100%">
+                    <thead>
+                        <tr>
+                            <th>Quiz</th>
+                            <th>Topic</th>
+                            <th>Date</th>
+                            <th>Grade</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Q1</td>
+                            <td>HTML</td>
+                            <td>1/21/25</td>
+                            <td>85</td>
+                        </tr>
+                        <tr>
+                            <td>Q2</td>
+                            <td>CSS</td>
+                            <td>1/28/25</td>
+                            <td>90</td>
+                        </tr>
+                        <tr>
+                            <td>Q3</td>
+                            <td>CSS</td>
+                            <td>2/4/25</td>
+                            <td>95</td>
+                        </tr>
+                        <tr>
+                            <td>Q4</td>
+                            <td>JavaScript</td>
+                            <td>2/11/25</td>
+                            <td>90</td>
+                        </tr>
+                        <tr>
+                            <td>Q5</td>
+                            <td>JavaScript</td>
+                            <td>2/18/25</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Q6</td>
+                            <td>Redux</td>
+                            <td>3/11/25</td>
+                            <td>95</td>
+                        </tr>
+                        <tr>
+                            <td>Q7</td>
+                            <td>Redux</td>
+                            <td>3/18/25</td>
+                            <td>85</td>
+                        </tr>
+                        <tr>
+                            <td>Q8</td>
+                            <td>Node</td>
+                            <td>3/25/25</td>
+                            <td>90</td>
+                        </tr>
+                        <tr>
+                            <td>Q9</td>
+                            <td>Node</td>
+                            <td>4/1/25</td>
+                            <td>98</td>
+                        </tr>
+                        <tr>
+                            <td>Q10</td>
+                            <td>Mongo</td>
+                            <td>4/8/25</td>
+                            <td>95</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={3}>Average</td>
+                            <td>92.3</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     );
 }
