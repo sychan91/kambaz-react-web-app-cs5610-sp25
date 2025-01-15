528d80f96f5df9a549f3b9bb1067908fe79d5fcb
sychan91 <c.saiyin@gmail.com>
2025-01-15 00:05:51 -0500

    Github repo added to TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab1" id="wd-a1">Lab 1</Link></li>
             <li><Link to="/Labs/Lab2" id="wd-a2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3" id="wd-a3">Lab 3</Link></li>
+            <li><a href="https://github.com/sychan91/kambaz-react-web-app-cs5610-sp25/tree/main" id="wd-github">My Github Repo</a></li>
         </ul>
     );
 }
