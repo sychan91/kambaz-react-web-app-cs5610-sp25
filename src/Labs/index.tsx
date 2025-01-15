1615e93f3a4ed913467ef27ffde70b0d233b505f
sychan91 <c.saiyin@gmail.com>
2025-01-15 00:08:38 -0500

    Name and section added to lab1 landing page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,7 @@ import TOC from "./TOC";
 export default function Labs() {
     return (
         <div id="wd-labs">
+            <h1>Sai Yin Chan, Section 01</h1>
             <h1>Labs</h1>
             <TOC />
             <Routes>
