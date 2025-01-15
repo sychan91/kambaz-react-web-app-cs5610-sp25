84b2fae990024977676750a28ac37fa683682b51
sychan91 <c.saiyin@gmail.com>
2025-01-15 17:20:21 -0500

    signin, signup, profile and navigation integrated

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 import Signin from "./Signin";
+import Signup from "./Signup";
+import Profile from "./Profile";
 
 export default function Account() {
     return (
         <div id="wd-account-screen">
             <h2>Account</h2>
-            <Signin />
+            <Routes>
+                <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
+                <Route path="Signin" element={<Signin />} />
+                <Route path="Signup" element={<Signup />} />
+                <Route path="Profile" element={<Profile />} />
+            </Routes>
         </div>
     );
 }
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,11 +1,17 @@
-// import { Route, Routes, Navigate } from "react-router";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Account from "./Account";
 
 export default function Kambaz() {
     return (
         <div id="wd-kambaz">
             <h1>Kambaz</h1>
-            <Account />
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="Account/*" element={<Account />} />
+                <Route path="Dashboard" element={<h3>Dashboard</h3>} />
+                <Route path="Courses" element={<h3>Courses</h3>} />
+                <Route path="Inbox" element={<h3>Inbox</h3>} />
+            </Routes>
         </div>
     );
 }
