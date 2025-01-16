d063475cae3cd66d6138a5856deab28252d396b4
sychan91 <c.saiyin@gmail.com>
2025-01-15 21:25:49 -0500

    Assignments implemented

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -2,6 +2,8 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from './Assignments';
+
 
 export default function Courses() {
     return (
@@ -15,7 +17,7 @@ export default function Courses() {
                             <Route path='/' element={<Navigate to='Home'/>} />
                             <Route path='Home' element={<Home />} />
                             <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
                         </Routes>
