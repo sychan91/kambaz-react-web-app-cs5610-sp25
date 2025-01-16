0c2ae138d1892ed6f2b7ed8301b99b38d6756eb0
sychan91 <c.saiyin@gmail.com>
2025-01-15 19:58:46 -0500

    Courses and Course Navigation implemented

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KambazNavigation from "./Navigation";
+import Courses from "./Courses";
 
 export default function Kambaz() {
     return (
@@ -17,7 +18,8 @@ export default function Kambaz() {
                             <Route path="/" element={<Navigate to="Account" />} />
                             <Route path="Account/*" element={<Account />} />
                             <Route path="Dashboard" element={<Dashboard />} />
-                            <Route path="Courses" element={<h3>Courses</h3>} />
+                            <Route path="Courses/:cid/*" element={<Courses />} />
+                            <Route path="Calendar" element={<h3>Calendar</h3>} />
                             <Route path="Inbox" element={<h3>Inbox</h3>} />
                         </Routes>
                     </td>    
