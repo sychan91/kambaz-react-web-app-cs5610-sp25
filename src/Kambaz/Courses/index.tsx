161ef445a438b14512fc00db5d25b3eb3063033e
sychan91 <c.saiyin@gmail.com>
2025-01-15 20:29:22 -0500

    Modules and Course Home implemented

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import CoursesNavigation from "./Navigation";
+import Modules from "./Modules";
+import Home from "./Home";
 
 export default function Courses() {
     return (
@@ -11,8 +13,8 @@ export default function Courses() {
                     <td valign='top'>
                         <Routes>
                             <Route path='/' element={<Navigate to='Home'/>} />
-                            <Route path='Home' element={<h2>Home</h2>} />
-                            <Route path="Modules" element={<h2>Modules</h2>} />
+                            <Route path='Home' element={<Home />} />
+                            <Route path="Modules" element={<Modules />} />
                             <Route path="Assignments" element={<h2>Assignments</h2>} />
                             <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
