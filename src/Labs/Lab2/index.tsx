1687bab27f7d4dbc842f498a437c934820ccac3e
sychan91 <c.saiyin@gmail.com>
2025-01-28 14:38:50 -0500

    2.2 completed

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,6 +11,7 @@ import Zindex from './Zindex';
 import Float from './Float';
 import GridLayout from './GridLayout';
 import Flex from './Flex';
+import ReactIcons from './ReactIcons';
 
 export default function Lab2() {
     return (
@@ -103,6 +104,9 @@ export default function Lab2() {
             </div>
             <div>
                 <Flex />
+            </div>
+            <div>
+                <ReactIcons />
             </div>
         </div>
     );
