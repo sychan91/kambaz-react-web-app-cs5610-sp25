908f31428d44508516d696b9d5f301a835838db4
sychan91 <c.saiyin@gmail.com>
2025-01-28 10:08:16 -0500

    lab2 2.1.(4,5) completed

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -25,6 +25,37 @@ export default function Lab2() {
                     feel
                 </p>
             </div>
+
+            <div id='wd-css-class-selectors'>
+                <h3>Class selectors</h3>
+                <p className='wd-class-selector'>
+                    Instead of using IDs to refer to elements, you
+                    can use an element's CLASS attribute
+                </p>
+                <h4 className='wd-class-selector'>
+                    This heading has same style as paragraph above
+                </h4>
+            </div>
+
+            <div id='wd-css-document-structure'>
+                <div className='wd-selector-1'>
+                    <h3>Document structure selector</h3>
+                    <div className='wd-selector-2'>
+                        Selectors can be combined to refer elements in particular
+                        places in the document
+                        <p className='wd-selector-3'>
+                            This paragraph's red background is referenced as
+                            <br/>
+                            .selector-2 .selector-3<br/>
+                            meaning the desendant of some ancestor.<br/>
+                            <span className='wd-selector-4'>
+                                Whereas this span is a direct child of its parent
+                            </span><br/>
+                                You can combine these relationships to create specific styles depending on the document structure
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
