17e7303aabe22804ba76be25cf614c5efa6aa3b4
sychan91 <c.saiyin@gmail.com>
2025-01-29 00:52:26 -0500

    2.3.15 completed

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -1,11 +1,16 @@
 import {
+  Button,
+  Col,
   Form,
   FormCheck,
   FormControl,
   FormGroup,
   FormLabel,
   FormSelect,
+  InputGroup,
+  Row,
 } from "react-bootstrap";
+import FormRange from "react-bootstrap/esm/FormRange";
 
 export default function BootstrapForms() {
   return (
@@ -68,6 +73,136 @@ export default function BootstrapForms() {
               label="Disabled checked switch checkbox input"
             />
           </FormGroup>
+        </Form>
+      </div>
+      <br />
+      <div id="wd-css-styling-range-and-sliders">
+        <h3>Range</h3>
+        <Form>
+          <FormGroup>
+            <FormLabel htmlFor="Example range">Example range</FormLabel>
+            <FormRange min="0" max="5" step="0.5" id="range1" />
+          </FormGroup>
+        </Form>
+      </div>
+      <br />
+      <div id="wd-css-styling-addons">
+        <h3>Addons</h3>
+        <Form>
+          <InputGroup>
+            <InputGroup.Text>$</InputGroup.Text>
+            <InputGroup.Text>0.00</InputGroup.Text>
+            <FormControl type="text" />
+          </InputGroup>
+        </Form>
+        <br />
+        <InputGroup>
+          <FormControl type="text" />
+          <InputGroup.Text>$</InputGroup.Text>
+          <InputGroup.Text>0.00</InputGroup.Text>
+        </InputGroup>
+      </div>
+      <br />
+      <div id="wd-css-responsive-forms-1">
+        <h3>Responsive Forms</h3>
+        <Form>
+          <FormGroup as={Row}>
+            <FormLabel column sm="2" htmlFor="email1">
+              Email
+            </FormLabel>
+            <Col sm="10">
+              <FormControl
+                type="text"
+                id="email1"
+                defaultValue="email@example.com"
+              />
+            </Col>
+          </FormGroup>
+          <br />
+          <FormGroup as={Row}>
+            <FormLabel column sm="2" htmlFor="password1">
+              Password
+            </FormLabel>
+            <Col sm="10">
+              <FormControl type="password" id="password1" />
+            </Col>
+          </FormGroup>
+          <br />
+          <FormGroup as={Row}>
+            <FormLabel column sm="2" htmlFor="textarea2">
+              Bio
+            </FormLabel>
+            <Col sm="10">
+              <FormControl as="textarea" id="textarea2" rows={3} />
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+      <br />
+      <div id="wd-css-responsive-forms-2">
+        <h3>Responsive Forms</h3>
+        <Form>
+          <FormGroup as={Row}>
+            <FormLabel column sm="2" htmlFor="r1">
+              Email
+            </FormLabel>
+            <Col sm="10">
+              <FormControl type="email" id="r1" />
+            </Col>
+          </FormGroup>
+          <br />
+          <FormGroup as={Row}>
+            <FormLabel column sm="2" htmlFor="r2">
+              Password
+            </FormLabel>
+            <Col sm="10">
+              <FormControl type="password" id="r2" />
+            </Col>
+            <br />
+          </FormGroup>
+          <br />
+          <fieldset>
+            <FormGroup as={Row}>
+              <Col as="legend" sm={2} className="radio-legend pt-0">
+                Radios
+              </Col>
+              <Col sm={10}>
+                <FormCheck
+                  type="radio"
+                  label="First radio"
+                  name="gridRadios"
+                  id="r3"
+                  value="option1"
+                  defaultChecked
+                />
+                <FormCheck
+                  type="radio"
+                  label="Second radio"
+                  name="gridRadios"
+                  id="r4"
+                  value="option2"
+                />
+                <FormCheck
+                  type="radio"
+                  label="Third disabled radio"
+                  name="gridRadios"
+                  id="r5"
+                  value="option3"
+                  disabled
+                />
+              </Col>
+            </FormGroup>
+          </fieldset>
+          <br />
+          <FormGroup as={Row}>
+            <Col sm={{ span: 10, offset: 2 }}>
+              <FormCheck type="checkbox" id="r6" label="Example checkbox" />
+            </Col>
+          </FormGroup>
+          <br />
+          <Button variant="primary" type="submit">
+            Sign in
+          </Button>
         </Form>
       </div>
     </div>
