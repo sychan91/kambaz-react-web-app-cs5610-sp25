fa644df2a26665c9fda17fbb23264f0f6e9b9f83
sychan91 <c.saiyin@gmail.com>
2025-01-29 12:52:19 -0500

    2.3.17 completed

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,14 +1,62 @@
-import { Link } from 'react-router-dom';
+import { Col, Nav, NavItem, NavLink, Row } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 
 export default function TOC() {
-    return (
-        <ul>
-            <li><Link to="/Labs" id="wd-a">Labs</Link></li>
-            <li><Link to="/Labs/Lab1" id="wd-a1">Lab 1</Link></li>
-            <li><Link to="/Labs/Lab2" id="wd-a2">Lab 2</Link></li>
-            <li><Link to="/Labs/Lab3" id="wd-a3">Lab 3</Link></li>
-            <li><a href="https://github.com/sychan91/kambaz-react-web-app-cs5610-sp25/tree/main" id="wd-github">My Github Repo</a></li>
-            <li><Link to="/Kambaz">Kambaz</Link></li>
-        </ul>
-    );
+  const { pathname } = useLocation();
+  return (
+    <Row>
+      <Col xs="2">
+        <Nav variant="pills" className="flex-column">
+          <Nav.Item>
+            <NavLink as={Link} to="/Labs" id="wd-a">
+              Labs
+            </NavLink>
+          </Nav.Item>
+          <NavItem>
+            <NavLink
+              as={Link}
+              to="/Labs/Lab1"
+              id="wd-a1"
+              className={pathname.includes("Lab1") ? "active" : ""}
+            >
+              Lab 1
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              as={Link}
+              to="/Labs/Lab2"
+              id="wd-a2"
+              className={pathname.includes("Lab2") ? "active" : ""}
+            >
+              Lab 2
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              as={Link}
+              to="/Labs/Lab3"
+              id="wd-a3"
+              className={pathname.includes("Lab3") ? "active" : ""}
+            >
+              Lab 3
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              href="https://github.com/sychan91/kambaz-react-web-app-cs5610-sp25/tree/main"
+              id="wd-github"
+            >
+              My Github Repo
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink as={Link} to="/Kambaz" id="wd-k">
+              Kambaz
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Col>
+    </Row>
+  );
 }
