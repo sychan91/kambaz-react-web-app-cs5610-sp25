3e841fdeb8009d6d334b50f56ff6f8983ec6de46
sychan91 <c.saiyin@gmail.com>
2025-01-29 13:04:54 -0500

    lab exercises complete

--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,15 +1,39 @@
-import { Tab, Tabs } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
+  Tab,
+  Tabs,
+} from "react-bootstrap";
 
 export default function BootstrapNavigation() {
   return (
     <div id="wd-css-navigating-with-tabs">
       <h2>Tabs</h2>
-      <Tabs defaultActiveKey="active">
+      <Tabs defaultActiveKey="active" className="mb-3">
         <Tab eventKey="active" title="Active"></Tab>
         <Tab eventKey="link1" title="Link"></Tab>
         <Tab eventKey="link2" title="Link"></Tab>
         <Tab eventKey="link3" title="Disabled" disabled></Tab>
       </Tabs>
+      <div id="wd-css-navigating-with-cards" className="mb-3">
+        <h2>Cards</h2>
+        <Card style={{ width: "18rem" }}>
+          <CardImg variant="top" src="images/stacked.jpg" />
+          <CardBody>
+            <CardTitle>Stacking Starship</CardTitle>
+            <CardText>
+              Stacking the most powerful rocket in history. Mars or Bust!
+            </CardText>
+            <Button variant="primary" href="#">
+              Boldly Go
+            </Button>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 }
