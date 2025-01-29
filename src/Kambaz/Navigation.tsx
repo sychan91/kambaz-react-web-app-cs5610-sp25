9a19b1adaf63bf2c63129bf462ed85fcc2c93850
sychan91 <c.saiyin@gmail.com>
2025-01-29 15:00:10 -0500

    kambaz navigation remake started

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,15 +1,93 @@
-import { Link } from 'react-router-dom';
+import { ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { GoBeaker } from "react-icons/go";
+import { BsEnvelopePaper } from "react-icons/bs";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { LuBookText } from "react-icons/lu";
+import { IoSpeedometerOutline } from "react-icons/io5";
+import { MdAccountCircle } from "react-icons/md";
 
 export default function KambazNavigation() {
-    return (
-        <div id='wd-kambaz-navigation'>
-            <a href='https://northeastern.edu/' id='wd-neu-link' target='_blank'>Northeastern</a><br/>
-            <Link to='/Kambaz/Account' id='wd-account-link'>Account</Link><br/>
-            <Link to='/Kambaz/Dashboard' id='wd-dashboard-link'>Dashboard</Link><br/>
-            <Link to='/Kambaz/Dashboard' id='wd-course-link'>Courses</Link><br/>
-            <Link to='/Kambaz/Calendar' id='wd-calendar-link'>Calendar</Link><br/>
-            <Link to='/Kambaz/Inbox' id='wd-inbox-link'>Inbox</Link><br/>
-            <Link to='/Labs' id='wd-labs-link'>Labs</Link><br/>
-        </div>
-    );
+  return (
+    <ListGroup
+      style={{ width: 90 }}
+      id="wd-kambaz-navigation"
+      className="rounded-0"
+    >
+      <ListGroup.Item
+        action
+        href="https://northeastern.edu/"
+        id="wd-neu-link"
+        target="_blank"
+        className="border-0 bg-black text-danger text-center wd-very-small wd-center"
+      >
+        <img src="/images/neulogo-2.png" className="wd-logo-sm" />
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Kambaz/Account"
+        id="wd-account-link"
+        className="border-0 bg-black text-white text-center wd-very-small"
+      >
+        <MdAccountCircle className="wd-icon-lg" />
+        <br />
+        Account
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Kambaz/Dashboard"
+        id="wd-dashboard-link"
+        className="border-0 bg-white text-danger text-center wd-very-small"
+      >
+        <IoSpeedometerOutline className="wd-icon-red wd-icon-md" />
+        Dashboard
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Kambaz/Dashboard"
+        id="wd-course-link"
+        className="border-0 bg-black text-white text-center wd-very-small"
+      >
+        <LuBookText className="wd-icon-red wd-icon-md" />
+        <br />
+        Courses
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Kambaz/Calendar"
+        id="wd-calendar-link"
+        className="border-0 bg-black text-white text-center wd-very-small"
+      >
+        <FaRegCalendarAlt className="wd-icon-red wd-icon-md" />
+        <br />
+        Calendar
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Kambaz/Inbox"
+        id="wd-inbox-link"
+        className="border-0 bg-black text-white text-center wd-very-small"
+      >
+        <BsEnvelopePaper className="wd-icon-red wd-icon-md" />
+        <br />
+        Inbox
+      </ListGroup.Item>
+
+      <ListGroup.Item
+        as={Link}
+        to="/Labs"
+        id="wd-labs-link"
+        className="border-0 bg-black text-white text-center wd-very-small"
+      >
+        <GoBeaker className="wd-icon-red wd-icon-md" />
+        <br />
+        Labs
+      </ListGroup.Item>
+    </ListGroup>
+  );
 }
--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -5,26 +5,19 @@ import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
 
 export default function Kambaz() {
-    return (
-        <div id="wd-kambaz">
-            <h1>Kambaz</h1>
-            <table>
-                <tr>
-                    <td valign="top">
-                        <KambazNavigation/>
-                    </td>
-                    <td valign='top'>
-                        <Routes>
-                            <Route path="/" element={<Navigate to="Account" />} />
-                            <Route path="Account/*" element={<Account />} />
-                            <Route path="Dashboard" element={<Dashboard />} />
-                            <Route path="Courses/:cid/*" element={<Courses />} />
-                            <Route path="Calendar" element={<h3>Calendar</h3>} />
-                            <Route path="Inbox" element={<h3>Inbox</h3>} />
-                        </Routes>
-                    </td>    
-                </tr>
-            </table>
-        </div>
-    );
+  return (
+    <div id="wd-kambaz">
+      <KambazNavigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Account" />} />
+          <Route path="Account/*" element={<Account />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/:cid/*" element={<Courses />} />
+          <Route path="Calendar" element={<h3>Calendar</h3>} />
+          <Route path="Inbox" element={<h3>Inbox</h3>} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
