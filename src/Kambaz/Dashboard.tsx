244ebdd476436fa49d92e4e3317f7284151c8cc5
sychan91 <c.saiyin@gmail.com>
2025-01-29 17:10:25 -0500

    Dashboard upgraded

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
@@ -6,82 +7,170 @@ export default function Dashboard() {
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <img src="/images/reactjs.webp" width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS1234 React JS </Link>
-            <p className="wd-dashboard-course-title">
-              Full Stack software developer </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5001 Intensive Foundations of Computer Science </Link>
-            <p className="wd-dashboard-course-title">
-              Introduction to Computer Science </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5002 Discrete Structures </Link>
-            <p className="wd-dashboard-course-title">
-              Learn about Discrete Structures </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5004 Object-Oriented Programming </Link>
-            <p className="wd-dashboard-course-title">
-              Introduction to OOP and Java </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5008 Datastructures & Algorithms </Link>
-            <p className="wd-dashboard-course-title">
-              Precuror to CS 5008 Algorithms </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5600 Database Management Systems </Link>
-            <p className="wd-dashboard-course-title">
-              DBMS </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
-        <br/>
-        <div className="wd-dashboard-course">
-          <img src='/images/neulogo.jpg' width={200} />
-          <div>
-            <Link className="wd-dashboard-course-link"
-                  to="/Kambaz/Courses/1234/Home"> CS5800 Algorithms </Link>
-            <p className="wd-dashboard-course-title">
-              Deep dive into Algorithms </p>
-            <Link to="/Kambaz/Courses/1234/Home"> Go </Link>
-          </div>
-        </div>
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card className="h-100 d-flex flex-column">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS1234 React JS
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Full Stack software developer
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5001 Foundations of Computer Science
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Introduction to Computer Science
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5002 Discrete Structures
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Learn about discrete structures
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5004 Object-Oriented Programming
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Introduction to OOP and Java
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5008 Data Structures & Algorithms
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Precursor to CS 5008 Algorithms
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5200 Database Management Systems
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    DBMS
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <Card.Img
+                  variant="top"
+                  width="100%"
+                  src="/images/reactjs.webp"
+                  height={160}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title">
+                    CS5800 Advanced Algorithms
+                  </Card.Title>
+                  <Card.Text className="wd-dashboard-course-description">
+                    Deep dive into algorithms
+                  </Card.Text>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
-);}
+  );
+}
