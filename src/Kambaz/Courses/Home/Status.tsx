ca7fb14f662e245a9d440b10ca8924602dfd8796
sychan91 <c.saiyin@gmail.com>
2025-01-29 23:49:03 -0500

    Course Home Page completed

--- src/Kambaz/Courses/Home/Status.tsx
+++ src/Kambaz/Courses/Home/Status.tsx
@@ -1,17 +1,68 @@
+import { Button } from "react-bootstrap";
+import { BiImport } from "react-icons/bi";
+import { FaCheckCircle } from "react-icons/fa";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { IoMdHome } from "react-icons/io";
+import { RiBarChart2Fill } from "react-icons/ri";
+import { TfiAnnouncement } from "react-icons/tfi";
+import { FaBell } from "react-icons/fa";
+
 export default function CourseStatus() {
-    return (
-        <div id='wd-course-status'>
-            <h2>Course Status</h2>
-            <button>Unpublish</button>
-            <button>Publish</button><br/>
-            <br/>
-            <button>Import Existing Content</button><br/>
-            <button>Import from Commons</button><br/>
-            <button>Choose Home Page</button><br/>
-            <button>View Course Stream</button><br/>
-            <button>New Announcement</button><br/>
-            <button>New Analytics</button><br/>
-            <button>View Course Notifications</button>
+  return (
+    <div
+      id="wd-course-status"
+      style={{ width: "250px" }}
+      className="wd-padding-right"
+    >
+      <h4>Course Status</h4>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button
+            variant="secondary"
+            size="sm"
+            className="w-100 align-items-center justify-content-center text-nowrap"
+          >
+            <MdDoNotDisturbAlt className="me-2 wd-f-small" />
+            Unpublish
+          </Button>
         </div>
-    );
+        <div className="w-50 pe-1">
+          <Button variant="success" size="sm" className="w-100 text-nowrap">
+            <FaCheckCircle className="me-2 fs-6" />
+            Publish
+          </Button>
+        </div>
+      </div>
+      <br />
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-6" />
+        Import Existing Content
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-6" />
+        Import from Commons
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <IoMdHome className="me-2 fs-6" />
+        Choose Home Page
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <RiBarChart2Fill className="me-2 fs-6" />
+        View Course Screen
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <TfiAnnouncement className="me-2 fs-6" />
+        New Announcement
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <RiBarChart2Fill className="me-2 fs-6" />
+        New Analytics
+      </Button>
+      <Button variant="secondary" size="sm" className="w-100 mt-1 text-start">
+        <FaBell className="me-2 fs-6" />
+        View Course Notifications
+      </Button>
+    </div>
+  );
 }
