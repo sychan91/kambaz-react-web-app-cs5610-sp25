7159c4cdbe77ed6fb1ede009c616fa8ee45654c6
sychan91 <c.saiyin@gmail.com>
2025-02-14 18:10:10 -0500

    3.3.11 complete

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -19,6 +19,9 @@ import FindIndex from "./FindIndex";
 import FilterFunction from "./FilterFunction";
 import JsonStringify from "./JsonStringify";
 import House from "./House";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+import Spreading from "./Spreading";
 
 export default function Lab3() {
   return (
@@ -45,6 +48,10 @@ export default function Lab3() {
       <FilterFunction />
       <JsonStringify />
       <House />
+      <TodoItem />
+      <br />
+      <TodoList />
+      <Spreading />
     </div>
   );
 }
