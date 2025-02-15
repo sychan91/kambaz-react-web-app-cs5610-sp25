dae6435e625c6bee53b5255c6a4653a707ef779e
sychan91 <c.saiyin@gmail.com>
2025-02-14 22:10:46 -0500

    3.5.1 complete

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -26,6 +26,10 @@ import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
 import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
 
 export default function Lab3() {
   return (
@@ -60,6 +64,18 @@ export default function Lab3() {
       <FunctionDestructing />
       <DestructingImports />
       <Classes />
+      <Styles />
+      <br />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
     </div>
   );
 }
