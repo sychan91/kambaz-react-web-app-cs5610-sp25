562c09a0c261d325cf1ab826c09303716085e7ec
sychan91 <c.saiyin@gmail.com>
2025-02-14 22:47:40 -0500

    lab complete

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -33,6 +33,7 @@ import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log("Hello World!");
   return (
     <div id="wd-lab3">
       <h2>Lab 3</h2>
@@ -80,6 +81,7 @@ export default function Lab3() {
       <br />
       <br />
       <PathParameters />
+      <br />
     </div>
   );
 }
