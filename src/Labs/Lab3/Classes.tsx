2e1f89ca0fa01e2df77581a93bf20821386c6893
sychan91 <c.saiyin@gmail.com>
2025-02-14 19:00:28 -0500

    3.4.1 started

---
+++ src/Labs/Lab3/Classes.tsx
@@ -0,0 +1,12 @@
+import "./Classes.css";
+export default function Classes() {
+  const color = "blue";
+  return (
+    <div>
+      <h2>Classes</h2>
+      <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+        Dynamic Blue background
+      </div>
+    </div>
+  );
+}
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -25,6 +25,7 @@ import Spreading from "./Spreading";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
 
 export default function Lab3() {
   return (
@@ -58,6 +59,7 @@ export default function Lab3() {
       <Destructing />
       <FunctionDestructing />
       <DestructingImports />
+      <Classes />
     </div>
   );
 }
