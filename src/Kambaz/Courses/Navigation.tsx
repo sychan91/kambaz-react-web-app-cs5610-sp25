6b6fa9d245d9fcf6ab8d0fd48e5d7b8765f30e67
sychan91 <c.saiyin@gmail.com>
2025-02-15 23:15:56 -0500

    more efficient implementation of dynamic course nav

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useParams } from "react-router-dom";
 
 export default function CoursesNavigation() {
   const { cid } = useParams();
-  const location = useLocation();
+  const { pathname } = useLocation();
   const links = [
     "Home",
     "Modules",
@@ -16,21 +16,16 @@ export default function CoursesNavigation() {
   ];
   return (
     <ListGroup id="wd-courses-navigation" className="wd rounded-0 wd-f-small">
-      {links.map((link) => {
-        const path = `/Kambaz/Courses/${cid}/${link}`;
-        return (
-          <ListGroup.Item
-            key={link}
-            className={`border-0 text-danger bg-color-white ${
-              location.pathname === path ? "active" : ""
-            }`}
-            as={Link}
-            to={path}
-          >
-            {link}
-          </ListGroup.Item>
-        );
-      })}
+      {links.map((link) => (
+        <ListGroup.Item
+          active={pathname.includes(link)}
+          className="border-0 text-danger bg-color-white"
+          as={Link}
+          to={`/Kambaz/Courses/${cid}/${link}`}
+        >
+          {link}
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
