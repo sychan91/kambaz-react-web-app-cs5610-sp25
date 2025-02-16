6f7e081fbf694e98dc2aabca65919a15049b2beb
sychan91 <c.saiyin@gmail.com>
2025-02-15 23:10:33 -0500

    course navigation dynamic

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,8 +1,9 @@
 import { ListGroup } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 export default function CoursesNavigation() {
   const { cid } = useParams();
+  const location = useLocation();
   const links = [
     "Home",
     "Modules",
@@ -15,15 +16,21 @@ export default function CoursesNavigation() {
   ];
   return (
     <ListGroup id="wd-courses-navigation" className="wd rounded-0 wd-f-small">
-      {links.map((link) => (
-        <ListGroup.Item
-          className="border-0 text-danger bg-color-white"
-          as={Link}
-          to={`/Kambaz/Courses/${cid}/${link}`}
-        >
-          {link}
-        </ListGroup.Item>
-      ))}
+      {links.map((link) => {
+        const path = `/Kambaz/Courses/${cid}/${link}`;
+        return (
+          <ListGroup.Item
+            key={link}
+            className={`border-0 text-danger bg-color-white ${
+              location.pathname === path ? "active" : ""
+            }`}
+            as={Link}
+            to={path}
+          >
+            {link}
+          </ListGroup.Item>
+        );
+      })}
     </ListGroup>
   );
 }
