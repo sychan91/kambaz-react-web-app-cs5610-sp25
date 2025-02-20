1e100c04acd42a36da85b67b11d22264acd01659
sychan91 <c.saiyin@gmail.com>
2025-02-19 23:23:33 -0500

    4.2.1.4 completed

---
+++ src/Labs/Lab4/ClickEvent.tsx
@@ -0,0 +1,35 @@
+const hello = () => {
+  alert("Hello World!");
+};
+
+const lifeIs = (good: string) => {
+  alert(`Life is ${good}`);
+};
+
+export default function ClickEvent() {
+  return (
+    <div id="wd-click-event">
+      <h2>Click Event</h2>
+      <button onClick={hello} id="wd-hello-world-click" className="me-2">
+        Hello World!
+      </button>
+      <button
+        onClick={() => lifeIs("Good!")}
+        id="wd-life-is-good-click"
+        className="me-2"
+      >
+        Life is Good!
+      </button>
+      <button
+        onClick={() => {
+          hello();
+          lifeIs("Great!");
+        }}
+        id="wd-life-is-great-click"
+      >
+        Life is Great!
+      </button>
+      <hr />
+    </div>
+  );
+}
--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,9 +1,20 @@
 import { Container } from "react-bootstrap";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
 export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
   return (
     <Container id="wd-lab4">
       <h2>Lab 4</h2>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
     </Container>
   );
 }
