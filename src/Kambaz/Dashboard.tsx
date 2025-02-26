1b5d99cae5ee7119c4227bd3332fc81ca55d477f
sychan91 <c.saiyin@gmail.com>
2025-02-26 18:41:37 -0500

    Dashboard: add, delete, edit, update implemented

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,12 +1,69 @@
 import { Button, Card, Col, Row } from "react-bootstrap";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "./Database";
 
 export default function Dashboard() {
-  const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    image: "/images/reactjs.jpg",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    description: "New Description",
+  });
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: new Date().getTime().toString() };
+    setCourses([...courses, newCourse]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourse(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>
+        New Course{" "}
+        <button
+          className="btn btn-primary float-end me-2"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          Add
+        </button>
+        <button
+          className="btn btn-warning float-end me-2"
+          onClick={updateCourse}
+          id="wd-update-course-click"
+        >
+          Update
+        </button>
+      </h5>
+      <br />
+      <input
+        value={course.name}
+        className="form-control mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <textarea
+        value={course.description}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <hr />
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
       </h2>{" "}
@@ -38,6 +95,26 @@ export default function Dashboard() {
                       {course.description}
                     </Card.Text>
                     <Button variant="primary">Go</Button>
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                      className="btn btn-danger float-end"
+                      id="wd-delete-course-click"
+                    >
+                      Delete
+                    </button>
+                    <button
+                      id="wd-edit-course-click"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setCourse(course);
+                      }}
+                      className="btn btn-warning float-end me-2"
+                    >
+                      Edit
+                    </button>
                   </Card.Body>
                 </Link>
               </Card>
