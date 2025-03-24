b4e328705638eb21d70743710041c3fc0c4ac874
sychan91 <c.saiyin@gmail.com>
2025-03-24 19:11:40 -0400

    5.2.4.7 complete

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import { FormControl } from "react-bootstrap";
+import { FormCheck, FormControl } from "react-bootstrap";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
 export default function WorkingWithArrays() {
   const API = `${REMOTE_SERVER}/lab5/todos`;
-  const [todo, setTodo] = useState({ id: 1 });
+  const [todo, setTodo] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-09-09",
+    completed: false,
+  });
   return (
     <div id="wd-working-with-arrays">
       <h3>Working with Arrays</h3>
@@ -45,6 +51,82 @@ export default function WorkingWithArrays() {
       >
         Create Todo
       </a>
+      <hr />
+      <h3>Deleting from an Array</h3>
+      <a
+        id="wd-retrieve-completed-todos"
+        className="btn btn-primary float-end mb-2"
+        href={`${API}/${todo.id}/delete`}
+      >
+        Delete Todo with ID
+      </a>
+      <FormControl
+        defaultValue={todo.id}
+        className="w-50"
+        onChange={(e) => setTodo({ ...todo, id: Number(e.target.value) })}
+      />
+      <hr />
+      <h3>Updating an Item in an Array</h3>
+      <a
+        href={`${API}/${todo.id}/title/${todo.title}`}
+        className="btn btn-primary float-end me-2"
+      >
+        Update Todo
+      </a>
+      <FormControl
+        defaultValue={todo.id}
+        className="w-25 float-start me-2"
+        onChange={(e) => setTodo({ ...todo, id: Number(e.target.value) })}
+      />
+      <FormControl
+        defaultValue={todo.title}
+        className="w-50 float-start"
+        onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+      />
+      <br />
+      <br />
+      <hr />
+      <h3>Updating Completed Field</h3>
+      <FormControl
+        defaultValue={todo.id}
+        className="w-25 float-start me-2 mb-2"
+        onChange={(e) => setTodo({ ...todo, id: Number(e.target.value) })}
+      />
+      <FormCheck
+        type="checkbox"
+        className="float-start"
+        label="Completed"
+        checked={todo.completed}
+        onChange={(e) => setTodo({ ...todo, completed: e.target.checked })}
+      />
+      <a
+        className="btn btn-primary float-end me-2"
+        href={`${API}/${todo.id}/completed/${todo.completed}`}
+      >
+        Update Completed
+      </a>
+      <br />
+      <br />
+      <hr />
+      <h3>Updating Description Field</h3>
+      <FormControl
+        defaultValue={todo.id}
+        className="w-25 float-start me-2 mb-2"
+        onChange={(e) => setTodo({ ...todo, id: Number(e.target.value) })}
+      />
+      <FormControl
+        className="w-50 float-start me-2"
+        defaultValue={todo.description}
+        onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+      />
+      <a
+        className="btn btn-primary float-end me-2"
+        href={`${API}/${todo.id}/description/${todo.description}`}
+      >
+        Update Description
+      </a>
+      <br />
+      <br />
       <hr />
     </div>
   );
