f99cf9d2129577f05b9691a762467097f58f067e
sychan91 <c.saiyin@gmail.com>
2025-03-25 15:41:42 -0400

    5.2.6.4 complete

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -29,7 +29,22 @@ export const removeTodo = async (todo: any) => {
   return response.data;
 };
 
+export const deleteTodo = async (todo: any) => {
+  const response = await axios.delete(`${TODOS_API}/${todo.id}`);
+  return response.data;
+};
+
 export const createTodo = async () => {
   const response = await axios.get(`${TODOS_API}/create`);
+  return response.data;
+};
+
+export const postTodo = async (todo: any) => {
+  const response = await axios.post(`${TODOS_API}`, todo);
+  return response.data;
+};
+
+export const updateTodo = async (todo: any) => {
+  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
   return response.data;
 };
