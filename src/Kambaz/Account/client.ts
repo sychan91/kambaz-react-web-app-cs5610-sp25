8b83e7ff4a499dece90447b43d0486276095e3c6
sychan91 <c.saiyin@gmail.com>
2025-03-28 00:43:16 -0400

    Revert "before removing large chunk of src/Kambaz/Dashboard.tsx"
    This reverts commit 2a949b6bb2b59cbcfb02f0bc38143b95a4f435bb.

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -35,10 +35,3 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
-
-export const findMyCourses = async () => {
-  const { data } = await axiosWithCredentials.get(
-    `${USERS_API}/current/courses`
-  );
-  return data;
-};
