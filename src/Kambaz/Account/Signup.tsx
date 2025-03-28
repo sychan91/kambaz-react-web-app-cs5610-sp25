f4892fa1613c6aad05c2b33ac20ae9bc2fb279ad
sychan91 <c.saiyin@gmail.com>
2025-03-27 20:23:38 -0400

    5.3.2.2 complete

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,33 +1,61 @@
 // import React from "react";
+import { useState } from "react";
 import { Button, FormControl } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as client from "./client";
+import { setCurrentUser } from "./reducer";
 
 export default function Signup() {
+  const [user, setUser] = useState<any>({});
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signup = async () => {
+    if (user.password !== verifyPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    try {
+      const currentUser = await client.signup(user);
+      dispatch(setCurrentUser(currentUser));
+      navigate("/Kambaz/Account/Profile");
+    } catch (err: any) {
+      alert("Username already exists");
+    }
+  };
   return (
     <div id="wd-signup-screen" className="wd-account-component-width">
       <h3>Sign up</h3>
-      <FormControl id="wd-username" placeholder="username" className="mb-2" />
+      <FormControl
+        id="wd-username"
+        placeholder="username"
+        className="mb-2"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
       <FormControl
         id="wd-password"
         type="password"
         placeholder="password"
         className="mb-2"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
       />
       <FormControl
         id="wd-password-verify"
         type="password"
         placeholder="verify password"
         className="mb-2"
+        onChange={(e) => setVerifyPassword(e.target.value)}
       />
       <Button
         variant="primary"
         size="sm"
         className="w-100 mb-2"
-        href="/Kambaz/Account/Profile"
+        onClick={signup}
       >
-        Sign in
+        Sign up
       </Button>
-      <Link to="/Kambaz/Account/Signin">Sign out</Link>
+      <Link to="/Kambaz/Account/Signin">Sign in</Link>
     </div>
   );
 }
--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -7,3 +7,8 @@ export const signin = async (credentials: any) => {
   const response = await axios.post(`${USERS_API}/signin`, credentials);
   return response.data;
 };
+
+export const signup = async (user: any) => {
+  const response = await axios.post(`${USERS_API}/signup`, user);
+  return response.data;
+};
