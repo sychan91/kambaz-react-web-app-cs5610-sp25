2a949b6bb2b59cbcfb02f0bc38143b95a4f435bb
sychan91 <c.saiyin@gmail.com>
2025-03-27 23:21:46 -0400

    before removing large chunk of src/Kambaz/Dashboard.tsx

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -4,13 +4,29 @@ import Dashboard from "./Dashboard";
 import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
 import "./styles.css";
-import { useState } from "react";
-import { db } from "./Database";
+import { useEffect, useState } from "react";
+// import { db } from "./Database";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import Session from "./Account/Session";
+// import * as client from "./Courses/client";
+import * as userClient from "./Account/client";
+import { useSelector } from "react-redux";
 
 export default function Kambaz() {
-  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [courses, setCourses] = useState<any[]>([]);
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const fetchCourses = async () => {
+    try {
+      const courses = await userClient.findMyCourses();
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchCourses();
+  }, [currentUser]);
+
   const [course, setCourse] = useState<any>({
     _id: "0",
     name: "New Course",
