1d91f2a9e4a5d3699a64ddb36a71214e32ded039
sychan91 <c.saiyin@gmail.com>
2025-04-06 00:52:57 -0400

    6.2.6.6 complete

--- src/Kambaz/Courses/People/Details.tsx
+++ src/Kambaz/Courses/People/Details.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
+import { FaCheck, FaUserCircle } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 import { useParams, useNavigate } from "react-router";
 // import { Link } from "react-router-dom";
 import * as client from "../../Account/client";
+import { FaPencil } from "react-icons/fa6";
 export default function PeopleDetails({
   fetchUsers,
 }: {
@@ -11,7 +12,18 @@ export default function PeopleDetails({
 }) {
   const { uid } = useParams();
   const [user, setUser] = useState<any>({});
+  const [name, setName] = useState("");
+  const [editing, setEditing] = useState(false);
   const navigate = useNavigate();
+
+  const saveUser = async () => {
+    const [firstName, lastName] = name.split(" ");
+    const updatedUser = { ...user, firstName, lastName };
+    await client.updateUser(updatedUser);
+    setUser(updatedUser);
+    setEditing(false);
+    navigate(`/Kambaz/Account/Users`);
+  };
 
   const deleteUser = async (uid: string) => {
     await client.deleteUser(uid);
@@ -42,8 +54,35 @@ export default function PeopleDetails({
       </div>
       <hr />
       <div className="text-danger fs-4 wd-name">
-        {" "}
-        {user.firstName} {user.lastName}{" "}
+        {!editing && (
+          <FaPencil
+            onClick={() => setEditing(true)}
+            className="float-end fs-5 mt-2 wd-edit"
+          />
+        )}
+        {editing && (
+          <FaCheck
+            onClick={() => saveUser()}
+            className="float-end fs-5 mt-2 me-2 wd-save"
+          />
+        )}
+        {!editing && (
+          <div className="wd-name" onClick={() => setEditing(true)}>
+            {user.firstName} {user.lastName}
+          </div>
+        )}
+        {user && editing && (
+          <input
+            className="form-control w-50 wd-edit-name"
+            defaultValue={`${user.firstName} ${user.lastName}`}
+            onChange={(e) => setName(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                saveUser();
+              }
+            }}
+          />
+        )}
       </div>
       <b>Roles:</b> <span className="wd-roles"> {user.role} </span> <br />
       <b>Login ID:</b> <span className="wd-login-id"> {user.loginId} </span>{" "}
